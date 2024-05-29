f663fc918e37e40b9d5512c7b16c61be11d7959e
Leoryu90 <thetein@gmail.com>
2024-05-29 19:17:27 +0900

    :tada: 20240529 자바 첫 수업

---
+++ src/Test.java
@@ -0,0 +1,24 @@
+public class Test {
+
+    /**
+     *
+     * @author awd
+     * @apiNote
+     *
+     **/
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        int c = a + b;
+
+        System.out.println("Hello World");
+
+        System.out.println(c);
+        System.out.println(c);
+        System.out.println(c);
+        System.out.println(c);
+        System.out.println(c);
+    }
+}
