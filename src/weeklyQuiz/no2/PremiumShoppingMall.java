b1a57e6e7d28ef784e3015b769e8a03c4b37aaab
Leoryu90 <thetein@gmail.com>
2024-06-20 03:39:48 +0900

    20240619 Java weekly-4

--- src/weeklyQuiz/no2/PremiumShoppingMall.java
+++ src/weeklyQuiz/no2/PremiumShoppingMall.java
@@ -10,8 +10,6 @@ public class PremiumShoppingMall extends ShoppingMall {
         for (int i = 0; i < arrayCount; i++) {
             if (products[i].getStock() < 10) {
                 return false;
-            }else if(products[i].getName().isEmpty()){
-                return false;
             }
         }
         return true;
