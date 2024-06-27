abe6a270dc79b565100c4ce9f8a95cd5fe27ffd7
Leoryu90 <thetein@gmail.com>
2024-06-27 21:58:12 +0900

    :memo: 20240627 Java weekly test-2

--- src/weeklyQuiz/no3/BookShelf.java
+++ src/weeklyQuiz/no3/BookShelf.java
@@ -6,20 +6,24 @@ import java.util.List;
 public class BookShelf<T> implements BookManager<T> {
     private List<Book<T>> books;
 
+    // 생성자
     public BookShelf() {
         this.books = new ArrayList<>();
     }
 
+    // Book 객체를 books 리스트에 삽입
     @Override
     public void addBook(Book<T> book) {
         books.add(book);
     }
 
+    // Book 객체를 books 리스트에서 제거
     @Override
     public void removeBook(Book<T> book) {
         books.remove(book);
     }
 
+    // Book 객체의 제목이 주어진 제목(title)을 포함하는지 확인
     @Override
     public List<Book<T>> searchByTitle(String title) {
         List<Book<T>> result = new ArrayList<>();
@@ -31,6 +35,7 @@ public class BookShelf<T> implements BookManager<T> {
         return result;
     }
 
+    // 저자로 검색 하는 메소드
     @Override
     public List<Book<T>> searchByAuthor(String author) {
         List<Book<T>> result = new ArrayList<>();
